9092e02d549ea3ac21b18ce931e55b59d75b46a9
tikvad <TikvaDuker@carmel6000.amitnet.org>
2021-10-26 19:15:54 +0300

    small changes

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import './App.css';
 import TopBar from './components/TopBar.jsx';
 import { ThemeProvider } from '@mui/material';
 import { myTheme } from './theme';
+import Movies from "./components/Movies";
+
 function App() {
   return (
     <div className="App">
       <ThemeProvider theme={myTheme}>
         <TopBar />
+        <Movies />
       </ThemeProvider>
     </div>
   );
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -6,7 +6,8 @@ export function MovieCard({ title, year, genre, poster }) {
     width: "30vh",
     cursor: "pointer",
     "&:hover": {
-      filter: "brightness(90%)"
+      filter: "brightness(80%)",
+      transition: '0.2s'
     }
   };
   return (
@@ -17,12 +18,14 @@ export function MovieCard({ title, year, genre, poster }) {
         alt={title}
         sx={{ height: "80%" }}
       />
+      <CardContent sx={{p: '1% 2% 0 2%'}}>
         <Typography variant="subtitle2">
             {title}
           </Typography>
           <Typography variant='caption'>
                 {genre} | {year}
           </Typography>
+      </CardContent>
     </Card>
   );
 }
