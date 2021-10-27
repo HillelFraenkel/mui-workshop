09e2e242e49b0d2e25168f45f91491573d94528c
Hillel Fraenkel <hillelfraenkel@carmel6000.amitnet.org>
2021-10-27 16:49:05 +0300

    practiceeeeee

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
 import './App.css';
 import TopBar from './components/TopBar.jsx';
-import { ThemeProvider, Box } from '@mui/material';
-import { myTheme } from './theme';
-
-import { useState } from 'react';
 function App() {
 
   return (
--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -1,57 +1,8 @@
-import React, { useState } from "react";
-import {
-  AppBar,
-  Typography,
-  Box,
-  Toolbar,
-  ButtonGroup,
-  IconButton,
-  Badge,
-  Drawer,
-  Avatar
-} from "@mui/material";
-import {
-  Menu,
-  Search,
-  Notifications,
-  AccountCircle
-} from "@mui/icons-material";
-import { myTheme as theme } from "../theme";
+import React from "react";
+import { AppBar, Typography, Box, Toolbar } from "@mui/material";
+import {} from "@mui/icons-material";
+
 export default function TopBar(props) {
-  const [drawerOpen, setDrawerOpen] = useState(false);
-  const [anchorEl, setAnchorEl] = useState(null);
-  const [alerts, setAlerts] = useState([
-    "More human twins are being born now than ever before",
-    "The first person convicted of speeding was going eight mph",
-    "'New car smell' is the scent of dozens of chemicals",
-    "The world's smallest reptile was first reported in 2021",
-    "Some sea snakes can breathe through their skin",
-    "The heads on Easter Island have bodies",
-    "The moon has moonquakes",
-    "Arachibutyrophobia is the fear of having peanut butter stick to the roof of your mouth",
-    "The wood frog can hold its pee for up to eight months",
-    "The feeling of getting lost inside a mall is known as the Gruen transfer",
-    "Pineapple works as a natural meat tenderizer"
-  ]);
-
-  const handleUserMenuOpen = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleUserMenuClose = () => {
-    setAnchorEl(null);
-  };
-
-  const alertHandler = () => {
-    if (alerts.length > 0) {
-      alert(alerts[alerts.length - 1]);
-      let changeArr = [...alerts];
-      changeArr.pop();
-      setAlerts([...changeArr]);
-    } else {
-      alert("no new notifications :(");
-    }
-  };
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="fixed">
